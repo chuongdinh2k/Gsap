2165b8ff1cc15d7f656ae40d3d52198f219a1915
chuongdinh2k <dinhvanchuong2k@gmail.com>
2020-12-26 17:52:45 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ // Phone svg animation
+
+    // var tlPhone = gsap.timeline({defaults:{duration:0.7,ease:Back.easeInOut.config(2),opacity:0}})
+    // tlPhone.from(".bg-card",{delay:1,scale:.2,transformOrigin:'center'},"=.2")
+    //       .from(".layer-card",{delay:0.9,scale:.2,transformOrigin:'center'})
+    //       .from(".icon",{delay:0.5,scale:.2,transformOrigin:'center'},"=.2")
+    //       .from(".download-android",{delay:0.3,scale:.2,transformOrigin:'center'},"=.2")
+    //       .from(".download-apple",{delay:0.1,scale:.2,transformOrigin:'center'},"=.2")
